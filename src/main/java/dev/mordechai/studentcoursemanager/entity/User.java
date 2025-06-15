ef118d077b4006da1b15e09e76fa3786843dbf52
MordechaiDavid <mordechai.y.david@gmail.com>
2025-06-15 18:58:22 +0300

    Add user entity and configuration files for Spring Boot application

---
+++ src/main/java/dev/mordechai/studentcoursemanager/entity/User.java
@@ -0,0 +1,32 @@
+package dev.mordechai.studentcoursemanager.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(length = 255)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_type", nullable = false)
+    private UserType userType;
+
+    @Column(name = "special_key", unique = true, length = 255)
+    private String specialKey;
+}
---
+++ src/main/java/dev/mordechai/studentcoursemanager/entity/UserType.java
@@ -0,0 +1,6 @@
+package dev.mordechai.studentcoursemanager.entity;
+
+public enum UserType {
+    ADMIN,
+    STUDENT,
+}
